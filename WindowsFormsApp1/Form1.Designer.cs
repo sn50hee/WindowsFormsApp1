b3699f43221ae44d54121e8120a1f4d99ebc2d3b
SH Yoon <sn50hee@gmail.com>
2023-11-06 16:57:37 +0900

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_print = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_print
+            // 
+            this.textBox_print.Location = new System.Drawing.Point(12, 12);
+            this.textBox_print.Multiline = true;
+            this.textBox_print.Name = "textBox_print";
+            this.textBox_print.Size = new System.Drawing.Size(426, 426);
+            this.textBox_print.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 450);
+            this.Controls.Add(this.textBox_print);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_print;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //MessageBox.Show("안녕하세요?");
+
+            /*
+             텍스트 박스에 메시지 출력
+             */
+            textBox_print.Text = "이것은\r\n멀티라인\r\n텍스트박스\r\n인데요.";
+
+
+        }
+    }
+}

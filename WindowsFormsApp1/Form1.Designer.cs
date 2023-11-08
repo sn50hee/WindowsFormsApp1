5f6208138f526fbff6d41df0ed8b89460fdbf75b
Hyungjin <2hyungjin3@gmail.com>
2023-11-08 18:50:29 +0900

    석희님 코드 + 한 글자 삭제, 전체 삭제, +-기호 추가

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -49,11 +49,12 @@ namespace WindowsFormsApp1
             // 
             this.textBox_print.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox_print.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.textBox_print.Location = new System.Drawing.Point(16, 151);
+            this.textBox_print.Location = new System.Drawing.Point(20, 181);
+            this.textBox_print.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_print.Multiline = true;
             this.textBox_print.Name = "textBox_print";
             this.textBox_print.ReadOnly = true;
-            this.textBox_print.Size = new System.Drawing.Size(310, 41);
+            this.textBox_print.Size = new System.Drawing.Size(388, 49);
             this.textBox_print.TabIndex = 2;
             // 
             // button_input
@@ -63,9 +64,10 @@ namespace WindowsFormsApp1
             this.button_input.FlatAppearance.BorderSize = 2;
             this.button_input.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_input.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button_input.Location = new System.Drawing.Point(233, 206);
+            this.button_input.Location = new System.Drawing.Point(291, 247);
+            this.button_input.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_input.Name = "button_input";
-            this.button_input.Size = new System.Drawing.Size(93, 94);
+            this.button_input.Size = new System.Drawing.Size(116, 113);
             this.button_input.TabIndex = 1;
             this.button_input.Text = "계산";
             this.button_input.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
@@ -76,19 +78,21 @@ namespace WindowsFormsApp1
             // 
             this.textBox_input.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox_input.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.textBox_input.Location = new System.Drawing.Point(16, 59);
+            this.textBox_input.Location = new System.Drawing.Point(20, 71);
+            this.textBox_input.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_input.Multiline = true;
             this.textBox_input.Name = "textBox_input";
-            this.textBox_input.Size = new System.Drawing.Size(311, 86);
+            this.textBox_input.Size = new System.Drawing.Size(389, 103);
             this.textBox_input.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Elice DigitalCoding ver.H", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(89, 22);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(111, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(166, 22);
+            this.label1.Size = new System.Drawing.Size(133, 29);
             this.label1.TabIndex = 3;
             this.label1.Text = "심플한 계산기";
             // 
@@ -99,9 +103,10 @@ namespace WindowsFormsApp1
             this.plus.FlatAppearance.BorderSize = 0;
             this.plus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.plus.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.plus.Location = new System.Drawing.Point(16, 256);
+            this.plus.Location = new System.Drawing.Point(20, 307);
+            this.plus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.plus.Name = "plus";
-            this.plus.Size = new System.Drawing.Size(93, 44);
+            this.plus.Size = new System.Drawing.Size(116, 53);
             this.plus.TabIndex = 4;
             this.plus.Text = "+";
             this.plus.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
@@ -115,9 +120,10 @@ namespace WindowsFormsApp1
             this.minus.FlatAppearance.BorderSize = 0;
             this.minus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.minus.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.minus.Location = new System.Drawing.Point(124, 256);
+            this.minus.Location = new System.Drawing.Point(155, 307);
+            this.minus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.minus.Name = "minus";
-            this.minus.Size = new System.Drawing.Size(93, 44);
+            this.minus.Size = new System.Drawing.Size(116, 53);
             this.minus.TabIndex = 5;
             this.minus.Text = "-";
             this.minus.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
@@ -131,9 +137,10 @@ namespace WindowsFormsApp1
             this.mult.FlatAppearance.BorderSize = 0;
             this.mult.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.mult.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.mult.Location = new System.Drawing.Point(16, 306);
+            this.mult.Location = new System.Drawing.Point(20, 367);
+            this.mult.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.mult.Name = "mult";
-            this.mult.Size = new System.Drawing.Size(93, 44);
+            this.mult.Size = new System.Drawing.Size(116, 53);
             this.mult.TabIndex = 6;
             this.mult.Text = "*";
             this.mult.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
@@ -147,9 +154,10 @@ namespace WindowsFormsApp1
             this.div.FlatAppearance.BorderSize = 0;
             this.div.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.div.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.div.Location = new System.Drawing.Point(124, 306);
+            this.div.Location = new System.Drawing.Point(155, 367);
+            this.div.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.div.Name = "div";
-            this.div.Size = new System.Drawing.Size(93, 44);
+            this.div.Size = new System.Drawing.Size(116, 53);
             this.div.TabIndex = 7;
             this.div.Text = "/";
             this.div.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
@@ -163,9 +171,10 @@ namespace WindowsFormsApp1
             this.remind.FlatAppearance.BorderSize = 0;
             this.remind.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.remind.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.remind.Location = new System.Drawing.Point(232, 306);
+            this.remind.Location = new System.Drawing.Point(290, 367);
+            this.remind.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.remind.Name = "remind";
-            this.remind.Size = new System.Drawing.Size(93, 44);
+            this.remind.Size = new System.Drawing.Size(116, 53);
             this.remind.TabIndex = 8;
             this.remind.Text = "%";
             this.remind.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
@@ -179,13 +188,15 @@ namespace WindowsFormsApp1
             this.del_all.FlatAppearance.BorderSize = 0;
             this.del_all.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.del_all.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.del_all.Location = new System.Drawing.Point(15, 206);
+            this.del_all.Location = new System.Drawing.Point(19, 247);
+            this.del_all.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.del_all.Name = "del_all";
-            this.del_all.Size = new System.Drawing.Size(93, 44);
+            this.del_all.Size = new System.Drawing.Size(116, 53);
             this.del_all.TabIndex = 9;
             this.del_all.Text = "C";
             this.del_all.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.del_all.UseVisualStyleBackColor = false;
+            this.del_all.Click += new System.EventHandler(this.del_all_Click);
             // 
             // del_one
             // 
@@ -194,13 +205,15 @@ namespace WindowsFormsApp1
             this.del_one.FlatAppearance.BorderSize = 0;
             this.del_one.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.del_one.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.del_one.Location = new System.Drawing.Point(125, 206);
+            this.del_one.Location = new System.Drawing.Point(156, 247);
+            this.del_one.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.del_one.Name = "del_one";
-            this.del_one.Size = new System.Drawing.Size(93, 44);
+            this.del_one.Size = new System.Drawing.Size(116, 53);
             this.del_one.TabIndex = 10;
             this.del_one.Text = "<-";
             this.del_one.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.del_one.UseVisualStyleBackColor = false;
+            this.del_one.Click += new System.EventHandler(this.del_one_Click);
             // 
             // sign
             // 
@@ -209,13 +222,15 @@ namespace WindowsFormsApp1
             this.sign.FlatAppearance.BorderSize = 0;
             this.sign.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.sign.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.sign.Location = new System.Drawing.Point(232, 356);
+            this.sign.Location = new System.Drawing.Point(290, 427);
+            this.sign.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sign.Name = "sign";
-            this.sign.Size = new System.Drawing.Size(93, 44);
+            this.sign.Size = new System.Drawing.Size(116, 53);
             this.sign.TabIndex = 11;
             this.sign.Text = "+ / -";
             this.sign.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.sign.UseVisualStyleBackColor = false;
+            this.sign.Click += new System.EventHandler(this.sign_Click);
             // 
             // history
             // 
@@ -224,9 +239,10 @@ namespace WindowsFormsApp1
             this.history.FlatAppearance.BorderSize = 0;
             this.history.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.history.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.history.Location = new System.Drawing.Point(16, 356);
+            this.history.Location = new System.Drawing.Point(20, 427);
+            this.history.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.history.Name = "history";
-            this.history.Size = new System.Drawing.Size(200, 44);
+            this.history.Size = new System.Drawing.Size(250, 53);
             this.history.TabIndex = 12;
             this.history.Text = "내역 확인";
             this.history.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
@@ -234,10 +250,10 @@ namespace WindowsFormsApp1
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.ClientSize = new System.Drawing.Size(347, 419);
+            this.ClientSize = new System.Drawing.Size(434, 503);
             this.Controls.Add(this.history);
             this.Controls.Add(this.sign);
             this.Controls.Add(this.del_one);
@@ -251,6 +267,7 @@ namespace WindowsFormsApp1
             this.Controls.Add(this.textBox_input);
             this.Controls.Add(this.button_input);
             this.Controls.Add(this.textBox_print);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
